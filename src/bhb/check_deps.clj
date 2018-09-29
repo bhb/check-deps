18521a58ce9e408077a3fd562e10cd70dcbd98b5
Ben Brinckerhoff <ben@bbrinck.com>
2018-09-29 13:01:19 -0600

    Use spell-spec

--- src/bhb/check_deps.clj
+++ src/bhb/check_deps.clj
@@ -1,11 +1,28 @@
 (ns bhb.check-deps
   (:require [clojure.edn :as edn]
-            [clojure.tools.deps.alpha.specs]
             [clojure.spec.alpha :as s]
             [spell-spec.alpha :as spell]
             [expound.alpha :as expound]
+            [spell-spec.expound]
+            [clojure.tools.deps.alpha.specs :as specs]
             [clojure.string :as string])
   (:gen-class))
+
+(s/def :mvn/coord (spell/keys :req [:mvn/version] :opt-un [::specs/path ::specs/exclusions]))
+(s/def :local/coord (spell/keys :req-un [:local/root] :opt-un [::specs/path]))
+(s/def :git/coord (spell/keys :req [:git/url]))
+(s/def ::specs/resolve-args (spell/keys :opt-un [::specs/extra-deps ::specs/override-deps ::specs/default-deps]))
+(s/def ::specs/classpath-args (spell/keys :opt-un [::specs/classpath-overrides ::specs/extra-paths]))
+(s/def ::specs/deps-map (spell/keys :opt-un [::specs/paths ::specs/deps ::specs/aliases]))
+
+(defn length->threshold [len]
+  (condp #(<= %2 %1) len
+    4 1
+    5 1
+    6 2
+    11 3
+    20 4
+    (int (* 0.2 len))))
 
 (defn -main []
   (let [form (try
@@ -16,11 +33,11 @@
                  ::none))]
     (when-not (= ::none form)
       (binding [s/*explain-out* (expound/custom-printer
-                                 {:show-valid-values?	 true
-                                  :print-specs? false
-                                  :theme :figwheel-theme})]
+                                 {:print-specs? false
+                                  :theme :figwheel-theme})
+                spell/*length->threshold* length->threshold]
         (-> (s/explain-str
-             :clojure.tools.deps.alpha.specs/deps-map
+             ::specs/deps-map
              form)
             (string/replace #"\-+ Spec failed \-+.*\n" "")
             (println))))))
