1c6b740aa471eafad8a94f15c6b787194a943245
Ben Brinckerhoff <ben@bbrinck.com>
2018-09-29 12:43:40 -0600

    Uses expound for printing result

--- src/bhb/check_deps.clj
+++ src/bhb/check_deps.clj
@@ -1,7 +1,10 @@
 (ns bhb.check-deps
   (:require [clojure.edn :as edn]
             [clojure.tools.deps.alpha.specs]
-            [clojure.spec.alpha :as s])
+            [clojure.spec.alpha :as s]
+            [spell-spec.alpha :as spell]
+            [expound.alpha :as expound]
+            [clojure.string :as string])
   (:gen-class))
 
 (defn -main []
@@ -12,6 +15,12 @@
                  (println (.getMessage e))
                  ::none))]
     (when-not (= ::none form)
-      (s/explain
-       :clojure.tools.deps.alpha.specs/deps-map
-       form))))
+      (binding [s/*explain-out* (expound/custom-printer
+                                 {:show-valid-values?	 true
+                                  :print-specs? false
+                                  :theme :figwheel-theme})]
+        (-> (s/explain-str
+             :clojure.tools.deps.alpha.specs/deps-map
+             form)
+            (string/replace #"\-+ Spec failed \-+.*\n" "")
+            (println))))))
