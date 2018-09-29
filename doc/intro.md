a082f53bf5b1984c50c282788801ba78097d11c5
Ben Brinckerhoff <ben@bbrinck.com>
2018-09-29 11:59:33 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to check-deps
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/bhb/check_deps.clj
@@ -0,0 +1,7 @@
+(ns bhb.check-deps
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
