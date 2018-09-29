2cb8429e8db954feeb616b19ee3119cc956a72a4
Ben Brinckerhoff <ben@bbrinck.com>
2018-09-29 12:14:30 -0600

    Explains output

--- src/bhb/check_deps.clj
+++ src/bhb/check_deps.clj
@@ -1,7 +1,17 @@
 (ns bhb.check-deps
+  (:require [clojure.edn :as edn]
+            [clojure.tools.deps.alpha.specs]
+            [clojure.spec.alpha :as s])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; TODO - real command line parsing
+  (let [filename (first args)
+        s (slurp (or filename "deps.edn"))
+        form (edn/read-string s)]
+    (s/explain
+     :clojure.tools.deps.alpha.specs/deps-map
+     form
+     )
+    ))
